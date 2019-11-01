fc8a8eeab13e3e060ffe725871df984e1b4edc58
Alex Ballera <alexballera@gmail.com>
2019-11-01 18:11:08 -0300

    Agregado componente search

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -13,9 +13,17 @@ export class SpotifyService {
   getNewReleases = () => {
 
     const headers = new HttpHeaders({
-      Authorization: 'Bearer BQDl0odFFq_-dPdqelsLby9AwAQqw0zk08ub6lAoyak_wYL6Dr2ZJDxOVS9uydVxjv1DGuFkkhVKesHUjwA'
+      Authorization: 'Bearer BQAUd34BRKd--YMg0iLcxmrYXFXaxay_axxFM6wHntfxlnZvF96BCzy_Ewb7IXAvZ00WrAlQGE3PxfwHQrc'
     });
 
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers});
+  }
+
+  getArtista = (termino: string) => {
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer BQAUd34BRKd--YMg0iLcxmrYXFXaxay_axxFM6wHntfxlnZvF96BCzy_Ewb7IXAvZ00WrAlQGE3PxfwHQrc'
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, {headers});
   }
 }
